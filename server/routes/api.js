c94cb17fd9dee7600b091eb5bf59d42728d313bb
4875676f204775696c6c61756d65 <hugo.guillaume@efrei.net>
2020-10-20 11:18:58 +0200

    Mise a jour routes.

--- server/routes/api.js
+++ server/routes/api.js
@@ -244,34 +244,174 @@ router.post('/register/administratif', async (req, res) => {
 
     } else {
 
-        res.status(400).json({ message: 'Not found.' })
+        res.status(400).json({ message: 'Bad request. pseudo deja pris.' })
 
     }
 
 })
 
-router.post('/add/patient', async (req,res) => {
+router.post('/add/patient', async (req, res) => {
 
     const prenom = req.body.prenom
     const nom = req.body.nom
     const nsc = req.body.nsc
     const age = req.body.age
-    const sexe = req.body.sexe 
-    const birthDate = req.body.birthDate
-    const birthPlace = req.body.birthPlace
+    const sexe = req.body.sexe  //true = homme et false == femme
+    const birthDate = req.body.naissance
+    const birthPlace = req.body.lieu
     const tel = req.body.tel
     const adresse = req.body.adresse
     const profession = req.body.profession
-    const mutuelle = req.body.mutuelle 
+    const mutuelle = req.body.mutuelle
     const medecin = req.body.medecin
     const antecedant = req.body.antecedant
     const urgence = req.body.urgence
     const sport = req.body.sport
-    const enfant = req.body.enfant 
+    const enfant = req.body.enfant
     const service = req.body.service
 
+    let validation = true
+
+    const verif_req_pseudo = "SELECT nsc FROM patient"
+
+    const verif_pseudo = await db.query({
+        text: verif_req_pseudo
+    })
+
+    for (i = 0; i < verif_pseudo.rowCount; i++) {
+
+        if (nsc == verif_pseudo.rows[i].nsc) {
+            validation = false
+        }
+
+    }
+
+    //Tester cette partie apres avoir fait un service ...
+
+    let serviceId = -1
+
+    const verif_req_service = "SELECT id,nom FROM " + service
+
+    const verif_service = await db.query({
+        text: verif_req_service
+    })
+
+    for (i = 0; i < verif_service.rowCount; i++) {
+
+        if (service == verif_service.rows[i].nom) {
+            serviceId = verif_service.rows[i].id
+        }
+
+    }
+
+    if (serviceId == -1) {
+        validation = false
+    }
+
+    let medecinId = -1
+
+    const verif_req_med = "SELECT id,nom FROM medecin"
+
+    const verif_med = await db.query({
+        text: verif_req_med
+    })
+
+    for (i = 0; i < verif_med.rowCount; i++) {
+
+        if (medecin == verif_med.rows[i].nom) {
+            medecinId = verif_med.rows[i].id
+        }
+
+    }
+
+    if (medecinId == -1) {
+
+        validation = false
+
+    }
+
+    if (validation === true) {
+
+        const requete = "INSERT INTO patient(nom,prenom,nsc,age,sexe,lieu,tel,adresse,profession,mutuelle,antecedent,sport,medecin,service,enfant,urgence,naissance) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING *"
+
+        //const requete = "INSERT INTO patient(nom,prénom,nsc,age,sexe) VALUES($1,$2,$3,$4,$5)"
+        const result = await db.query({
+            text: requete,
+            values: [nom, prenom, nsc, age, sexe, birthPlace, tel, adresse, profession, mutuelle, antecedant, sport, medecinId, serviceId, enfant, urgence, birthDate]
+        })
+
+        if (result.rowCount == 0) {
+            res.status(400).json({ message: 'Erreur lors de la creation du compte. Reessayer.' })
+        } else {
+            res.json(result.rows)
+        }
+
+    } else {
+
+        res.status(400).json({ message: 'Not found.' })
+
+    }
 
 
 })
+
+router.post(('/add/service'), async (req, res) => {
+
+    const nom = req.body.nom
+    const capacite = req.body.capacite
+    const occupation = req.body.occupe
+    const chef = req.body.chef
+
+    if (typeof nom == String && nom == "") {
+
+        let medecinId = -1
+
+        const verif_req_med = "SELECT id,pseudo FROM medecin"
+
+        const verif_med = await db.query({
+            text: verif_req_med
+        })
+
+        for (i = 0; i < verif_med.rowCount; i++) {
+
+            if (chef == verif_med.rows[i].pseudo) {
+                medecinId = verif_med.rows[i].id
+            }
+
+        }
+
+        if (medecinId == -1) {
+
+            validation = false
+
+        }
+
+        if (validation === true) {
+
+            const requete = "INSERT INTO patient(nom,capacite,occupe,chef) values($1,$2,$3,$4) RETURNING *"
+    
+            const result = await db.query({
+                text: requete,
+                values: [nom, capacite, occupation, medecinId]
+            })
+    
+            if (result.rowCount == 0) {
+                res.status(400).json({ message: 'Erreur lors de la creation du compte. Reessayer.' })
+            } else {
+                res.json(result.rows)
+            }
+    
+        } else {
+    
+            res.status(400).json({ message: 'Not found.' })
+    
+        }
+
+    }
+
+
+})
+
+
 
 module.exports = router
