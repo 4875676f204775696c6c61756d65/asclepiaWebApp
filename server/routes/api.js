fa561c55b2cc87c22d4cfb4b1ad5e2e8244ff247
4875676f204775696c6c61756d65 <hugo.guillaume@efrei.net>
2020-10-20 09:41:32 +0200

    changement mineur. Route add patient en cours.

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,6 @@ const router = express.Router()
 
 const bcrypt = require('bcrypt')
 const { Client } = require('pg')
-const { json, query } = require('express')
 
 const db = new Client({
     user: 'postgres',
@@ -64,16 +63,16 @@ router.get('/table/:nom', async (req, res) => {
 
         let requete
 
-        if(table == 'medecin'){
+        if (table == 'medecin') {
 
             requete = "SELECT id,nom,prenom,specialite,emplacement,numpro,numperso,service,pseudo FROM " + table
 
-        }else{
+        } else {
 
             requete = "SELECT * FROM " + table
 
-        } 
-        
+        }
+
 
         const result = await db.query({
             text: requete
@@ -120,9 +119,7 @@ router.post(('/add/index'), async (req, res) => {
 
 })
 
-router.post('/register/:role', async (req, res) => {
-
-    const role = req.params.role
+router.post('/register/medecin', async (req, res) => {
 
     const nom = req.body.nom
     const prenom = req.body.prenom
@@ -140,74 +137,141 @@ router.post('/register/:role', async (req, res) => {
 
     let validation = true
 
-    if(role == "medecin"){
+    const verif_req_pseudo = "SELECT pseudo FROM medecin"
 
-        const verif_req_pseudo = "SELECT pseudo FROM " + role
+    const verif_pseudo = await db.query({
+        text: verif_req_pseudo
+    })
 
-        const verif_pseudo = await db.query({
-            text: verif_req_pseudo
-        })
+    for (i = 0; i < verif_pseudo.rowCount; i++) {
 
-        for (i = 0; i < verif_pseudo.rowCount; i++) {
-
-            if (pseudo == verif_pseudo.rows[i].pseudo) {
-                validation = false
-            }
-
-        }
-
-        //Tester cette partie apres avoir fait un service ...
-        let serviceId = -1
-
-        const verif_req_service = "SELECT id,nom FROM " + service
-
-        const verif_service = await db.query({
-            text: verif_req_service
-        })
-
-        for (i = 0; i < verif_service.rowCount; i++) {
-
-            if (service == verif_service.rows[i].nom) {
-                serviceId = verif_service.rows[i].id
-            }
-
-        }
-
-        if(serviceId == -1){
+        if (pseudo == verif_pseudo.rows[i].pseudo) {
             validation = false
         }
-        
 
-        if (validation === true) {
+    }
 
-            const requete = "INSERT INTO medecin(nom,mdp,prenom,specialite,emplacement,numpro,numperso,service,pseudo) values($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *"
+    //Tester cette partie apres avoir fait un service ...
+    let serviceId = -1
 
-            const result = await db.query({
-                text: requete,
-                values: [nom,mdp,prenom,specialite,lieuDeTravail,numPro,numPerso,service,pseudo]
-            })
+    const verif_req_service = "SELECT id,nom FROM " + service
 
-            if (result.rowCount == 0) {
-                res.status(400).json({ message: 'Erreur lors de la creation du compte. Reessayer.' })
-            } else {
-                res.json(result.rows)
-            }
+    const verif_service = await db.query({
+        text: verif_req_service
+    })
 
-        } else {
+    for (i = 0; i < verif_service.rowCount; i++) {
 
-            res.status(400).json({ message: 'Not found.' })
-
+        if (service == verif_service.rows[i].nom) {
+            serviceId = verif_service.rows[i].id
         }
 
-    }else{
+    }
 
-        res.json({ message: 'inscription pour le statut medecin uniquement'})
+    if (serviceId == -1) {
+        validation = false
+    }
+
+
+    if (validation === true) {
+
+        const requete = "INSERT INTO medecin(nom,mdp,prenom,specialite,emplacement,numpro,numperso,service,pseudo) values($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *"
+
+        const result = await db.query({
+            text: requete,
+            values: [nom, mdp, prenom, specialite, lieuDeTravail, numPro, numPerso, service, pseudo]
+        })
+
+        if (result.rowCount == 0) {
+            res.status(400).json({ message: 'Erreur lors de la creation du compte. Reessayer.' })
+        } else {
+            res.json(result.rows)
+        }
+
+    } else {
+
+        res.status(400).json({ message: 'Not found.' })
 
     }
 
 
+
 })
 
+router.post('/register/administratif', async (req, res) => {
 
+    const nom = req.body.nom
+    const prenom = req.body.prenom
+    const pseudo = req.body.pseudo
+    let mdp = req.body.mdp
+    const poste = req.body.poste
+    const lieuDeTravail = req.body.emplacement
+    const numPro = req.body.numPro
+    const numPerso = req.body.numPerso
+
+    mdp = await bcrypt.hash(mdp, 10)
+
+    let validation = true
+
+    const verif_req_pseudo = "SELECT pseudo FROM administratif"
+
+    const verif_pseudo = await db.query({
+        text: verif_req_pseudo
+    })
+
+    for (i = 0; i < verif_pseudo.rowCount; i++) {
+
+        if (pseudo == verif_pseudo.rows[i].pseudo) {
+            validation = false
+        }
+
+    }
+
+    if (validation === true) {
+
+        const requete = "INSERT INTO administratif(nom,prénom,pseudo,mdp,poste,emplacement, numpro, numperso) values($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *"
+
+        const result = await db.query({
+            text: requete,
+            values: [nom, prenom, pseudo, mdp, poste, lieuDeTravail, numPro, numPerso]
+        })
+
+        if (result.rowCount == 0) {
+            res.status(400).json({ message: 'Erreur lors de la creation du compte. Reessayer.' })
+        } else {
+            res.json(result.rows)
+        }
+
+    } else {
+
+        res.status(400).json({ message: 'Not found.' })
+
+    }
+
+})
+
+router.post('/add/patient', async (req,res) => {
+
+    const prenom = req.body.prenom
+    const nom = req.body.nom
+    const nsc = req.body.nsc
+    const age = req.body.age
+    const sexe = req.body.sexe 
+    const birthDate = req.body.birthDate
+    const birthPlace = req.body.birthPlace
+    const tel = req.body.tel
+    const adresse = req.body.adresse
+    const profession = req.body.profession
+    const mutuelle = req.body.mutuelle 
+    const medecin = req.body.medecin
+    const antecedant = req.body.antecedant
+    const urgence = req.body.urgence
+    const sport = req.body.sport
+    const enfant = req.body.enfant 
+    const service = req.body.service
+
+
+
+})
 
 module.exports = router
